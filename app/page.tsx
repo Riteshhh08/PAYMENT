4f8505b25fbe9793fa62fdee4c960cd5a890843f
atharvakun <163377040+atharvakun@users.noreply.github.com>
2025-10-16 21:35:46 +0530

    Update footer GitHub link to Riteshhh08

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <Link
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://github.com/PremPrakashCodes"
+            href="https://github.com/Riteshhh08"
             target="_blank"
             rel="noopener noreferrer"
           >
